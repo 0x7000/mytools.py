d95bd5ae065337636404e572072a64ebf34b112c
command <rekreb81@gmail.com>
2020-05-31 03:00:18 +0300

    Add files via upload

--- wget.py
+++ wget.py
@@ -1,28 +1,19 @@
-#!/usr/bin/env python
 import os
 
 
-def main():
+def indir(adres, aranan):
     konum = os.getcwd()
-    klasor = "d 1"
-    dosya = ADRES.split("/")[-1]
-    tam_yol = konum+"/"+klasor
-    tam_dosya = konum+"/"+klasor+"/"+dosya
+    dosya = adres.split("/")[-1]
+    tam_yol = konum+"/pics/"+aranan
+    tam_dosya = konum+"/pics/"+aranan+"/"+dosya
     if not os.path.exists(tam_yol):
-        os.mkdir(klasor)
-    komut = "wget -q " + ADRES + " -P " + '"'+tam_yol+'"'
+        os.mkdir(tam_yol)
+    komut = "wget --tries=1 --timeout=1 --dns-timeout=1 -q " + adres + " -P " + '"'+tam_yol+'"'
     if not os.path.exists(tam_dosya):
         os.system(komut)
     if os.path.exists(tam_dosya):
         boyut = os.stat(tam_dosya).st_size
         if boyut == 0:
             os.remove(tam_dosya)
-        print(boyut)
     else:
-        print("dosya yok")
-
-
-ADRES = "https://n11scdn.akamaized.net/a1/450/ev-yasam/hesap-makinesi/casio-fx-82ms-bilimsel-fonksiyonlu-hesap-makinesi__0340777259406790.jpg"
-
-if __name__ == '__main__':
-    main()
+        print("indirilemedi.")
